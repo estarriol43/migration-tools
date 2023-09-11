1f5a27b18b7cc63e8184fdb8957e7b0808de6b89
Jian-Lin, Li <b07902103@ntu.edu.tw>
2023-09-11 16:10:27 +0800

    Add sync.sh

---
+++ sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NFS_FILE="
+    /mydata/some-tutorials
+    /mydata/linux/arch/x86/boot/bzImage
+    /mydata/edk2-sev/Build/OvmfX64/DEBUG_GCC5/FV/OVMF_CODE.fd
+    /mydata/edk2-sev/Build/OvmfX64/DEBUG_GCC5/FV/OVMF_VARS.fd
+    /mydata/some-tutorials/files/amd-sev/ramdisk.img
+"
+
+MYDATA_FILE="
+    /mydata/qemu-sev
+"
+
+for file in $NFS_FILE
+do
+    rsync --progress -avrzh $file /proj/ntucsie-PG0/estarriol/
+done
+
+for file in $MYDATA_FILE
+do
+    rsync --progress -avrzh $file 10.10.1.1:/mydata
+    rsync --progress -avrzh $file 10.10.1.2:/mydata
+done
+
